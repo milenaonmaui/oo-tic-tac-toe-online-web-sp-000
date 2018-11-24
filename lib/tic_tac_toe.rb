15ccb7f89cf989facfac89a06560629780bf132f
Milena Kari <milenakari@yahoo.com>
2018-11-24 18:51:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,6 +3,45 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
+  WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+ ]
+
+ def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(index, player = "X")
+  @board[index] = player
+end
+
+def position_taken?(index)
+  @board[index] == 'X' || @board[index] == 'O'
+end
+
+def valid_move?(index)
+  index.between?(0, 8) && !position_taken?(index)
+end
+
+def turn
+  puts 'Please enter 1-9:'
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(index)
+    move(index, current_player)
+    display_board
+  else
+    turn
+  end
+end
+
   def current_player
     turn_count % 2 ==0 ? "X" : "O"
   end
