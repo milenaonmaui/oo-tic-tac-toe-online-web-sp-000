0390871b44d0e422cad6687b4430937c6f22e5bf
Milena Kari <milenakari@yahoo.com>
2018-11-24 19:03:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,7 +87,7 @@ end
   def play
      turn until over?
     if won?
-       puts "Congratulations #{winner(board)}!"
+       puts "Congratulations #{winner}!"
     elsif draw?
        puts "Cat's Game!"
     end
